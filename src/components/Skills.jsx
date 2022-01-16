d2e54e37300f2273c5b3c58b6d553b447e73cd2f
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2022-01-17 00:30:46 +0100

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,9 +12,9 @@ const Skills = () => {
     return (
         <div>
             <section className='bg-gray-800' id="skills">
-      <div data-aos='fade-down' className="container px-5 py-10 mx-auto">
+      <div data-aos='fade-up' className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
+          <ChipIcon className="text-white w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills &amp; Technologies
           </h1>
