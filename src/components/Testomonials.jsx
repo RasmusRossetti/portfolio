e918cc2a093c46d36e6cd764c3329ee8844aa51c
Rasmus Rossetti <73909140+RasmusRossetti@users.noreply.github.com>
2022-10-25 20:53:51 +0200

    Update Testomonials.jsx

--- src/components/Testomonials.jsx
+++ src/components/Testomonials.jsx
@@ -18,7 +18,7 @@ const Testomonials = () => {
         </h1>
         <div   className="flex flex-wrap m-4">
           {testimonial.map((testimonial) => (
-            <div data-aos= 'fade-up-left'  className="p-4 md:w-1/2 w-full ">
+            <div data-aos= 'fade-up'  className="p-4 md:w-1/2 w-full ">
               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
                 <TerminalIcon className="block w-8 text-gray-500 mb-4" />
                 <p className=" leading-relaxed mb-6 text-white">{testimonial.quote}</p>
