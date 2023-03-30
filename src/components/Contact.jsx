43b658f476458c6672f612bd55728d03b086eb51
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2023-03-30 16:16:21 +0200

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,51 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React, { useState, useEffect } from "react"
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 export const Contact = () => {
-  
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [state, setstate] = useState();
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
 
   function encode(data) {
     return Object.keys(data)
       .map(
         (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
       )
-      .join("&");
+      .join("&")
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
+    if (!name || !email || !message) {
+      e.preventDefault()
+      alert("Please fill in the fields")
+      return
+    }
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
+      body: encode({ "form-name": "contact", name, email, message })
     })
-      
       .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+      .catch((error) => alert(error))
+    reset()
   }
 
-  const reset = () =>{
-    setstate('')
-  };
-
+  const reset = () => {
+    setEmail("")
+    setMessage("")
+    setName("")
+  }
 
   useEffect(() => {
-    Aos.init({duration: 2000,once: true});
-    }, []);
-  
-
+    Aos.init({ duration: 2000, once: true })
+  }, [])
 
   return (
     <section id="contact" className="bg-gray-900 relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div id='map' className=" lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe 
+        <div
+          id="map"
+          className=" lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative"
+        >
+          <iframe
             width="100%"
             height="100%"
             title="map"
@@ -56,7 +59,10 @@ export const Contact = () => {
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=+stockholm&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-          <div id='map-box' className=" bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+          <div
+            id="map-box"
+            className=" bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md"
+          >
             <div className=" lg:w-56  px-6">
               <h2 className=" title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
@@ -70,7 +76,10 @@ export const Contact = () => {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a href='rasmus.rossetti@hotmail.com' className="text-indigo-600 leading-relaxed">
+              <a
+                href="rasmus.rossetti@hotmail.com"
+                className="text-indigo-600 leading-relaxed"
+              >
                 rasmus.rossetti@hotmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
@@ -80,39 +89,40 @@ export const Contact = () => {
             </div>
           </div>
         </div>
-        <form data-aos='zoom-in-left'
-              data-aos-duration="500"
+        <form
+          data-aos="zoom-in-left"
+          data-aos-duration="500"
           netlify
           name="contact"
           onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
           <p className="text-white  leading-relaxed mb-5">
-          If you are interested to hire or develop something cool dont hesitate to contact me!
+            If you are interested to hire or develop something cool dont
+            hesitate to contact me!
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
               Name
             </label>
             <input
-             
               type="text"
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              value={state}
-              onChange={(e) => setName(e.target.value)  }
-            / >
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="relative mb-4">
             <label htmlFor="email" className="leading-7 text-sm text-gray-400">
               Email
             </label>
             <input
-              
-              value={state}
+              value={email}
               type="email"
               id="email"
               name="email"
@@ -122,28 +132,27 @@ export const Contact = () => {
           </div>
           <div className="relative mb-4">
             <label
-              
               htmlFor="message"
-              className="leading-7 text-sm text-gray-400">
+              className="leading-7 text-sm text-gray-400"
+            >
               Message
             </label>
             <textarea
-              
-              value={state}
+              value={message}
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
               onChange={(e) => setMessage(e.target.value)}
             />
           </div>
-          <button 
-            onClick={reset}
+          <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
             Submit
           </button>
         </form>
       </div>
     </section>
-  );
+  )
 }
