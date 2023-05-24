3909381a8f0b3ddb2a17b643cb319d925b12250e
Rasmus Rossetti <rasmus.rossetti@hotmail.com>
2023-05-24 17:42:40 +0200

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from "react"
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [text, setText] = useState("")
+  const helloWorld = () => {
+    alert("hello world")
+    setText("text")
+  }
+  if (!text) {
+    return <button onClick={helloWorld}>CLICK ME to show text</button>
+  }
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default Footer
